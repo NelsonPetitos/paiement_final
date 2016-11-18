b454456e18d6580561a2d71c00e4cc0bc7337d75
NelsonPetitos <nde_nelson@yahoo.fr>
2016-11-18 08:59:50 +0100

    First commit

---
+++ public/js/wearetechapi-v1.js
@@ -0,0 +1,141 @@
+function WRTechAPI(){
+    this.apiKey = "Qw2@ivd56njks&jso9bc#cxz9-caxm-0";
+    this.apiCallback = function(data){
+        console.log(data);
+    };
+}
+WRTechAPI.prototype.setApiKey = function(newKey){
+    this.apiKey = newKey;
+    console.log("new api key = "+ newKey);
+}
+
+WRTechAPI.prototype.setCloseButton = function(closeButton){
+    this.closeButton = closeButton;
+    this.setCloseButtonEventListener();
+}
+
+WRTechAPI.prototype.setValidateButton = function(validateButton){
+    this.validateButton = validateButton;
+    this.setValidateButtonEventListemer();
+}
+
+WRTechAPI.prototype.setApiButtonEventListener = function(){
+    let btns = document.querySelectorAll('[data-wearetechbtn]');
+    for(let i = 0; i < btns.length; i++){
+        console.log(btns[i]);
+    }
+    btns.forEach(function(btn){
+        if(btn.dataset.wearetechbtn != 0){
+            btn.addEventListener("click", function(){
+                var xhttp;
+                if(window.XDomainRequest){
+                    xhttp = new XDomainRequest();
+                    console.log("Le navigateur prend en compte le XDoamainRequest");
+                }else if(window.XMLHttpRequest){
+                    xhttp = new XMLHttpRequest();
+                    console.log("Le navigateur prend en compte le XMLHttpRequest");
+                }else{
+                    console.log("Le navigateur prend en compte ni le XMLHttpRequest ni le XDoamainRequest");
+                    return;
+                }
+
+                let amount = this.dataset.amount;
+                if(amount){
+                    // xhttp.withCredentials = true;
+                    // console.log(toto);
+                    xhttp.open("GET", "https://paiementback.herokuapp.com/initpopup/"+amount, true);
+                    xhttp.send();
+                }else{
+                    console.log("Set data-amount attribute to your button");
+                    return;
+                }
+
+                xhttp.onerror = function(){
+                    console.log("And error occur during the XmlHttpRequest communication");
+                }
+
+                xhttp.onload = function(){
+                    if(this.readyState == 4 && this.status == 200){
+                        //Il faut faire un controle avant d'inserer de nouveau le code suivant.
+                        document.body.insertAdjacentHTML('beforeend', this.responseText);
+                        // var coupe = this.responseText.split("<script>");
+                        // var javcsrpt = coupe[1].split("<\/script>");
+                        // eval(javcsrpt[0]);
+                        let closebtn = document.getElementById("wearetech_closemodal");
+                        if(closebtn){
+                            WAPI.setCloseButton(closebtn);
+                        }
+                        let valBtn = document.getElementById("wearetech_validate");
+                        if(valBtn){
+                            WAPI.setValidateButton(valBtn);
+                        }
+                    }
+                }
+            })
+        }else{
+            console.log("This button is not handle");
+        }
+    })
+}
+
+//TThis method will set the callback of the
+WRTechAPI.prototype.setApiCallback = function(resolve){
+    if(typeof resolve === 'function'){
+        this.apiCallback = resolve;
+    }
+}
+
+WRTechAPI.prototype.setValidateButtonEventListemer = function(){
+    this.validateButton.addEventListener("click", function () {
+        if(typeof io !== 'undefined'){
+            let socket = io.connect('https://paiementback.herokuapp.com/');
+            if(typeof socket !== 'undefined'){
+
+                let phone = document.getElementById('wearetech_phone_number').value;
+                let message = {phone: phone, code: 237, apiKey: WAPI.apiKey};
+                socket.emit('wearetechapi_client_emit', message);
+
+                //dismiss modal view after the connect has been send
+                let modaldiv = document.getElementById('wearetech_modal');
+                if(modaldiv){
+                    document.body.removeChild(modaldiv);
+                }else{
+                    console.log("there is not a block div with id='wearetech_modal'");
+                }
+
+                socket.on('wearetechapi_server_response', function(result) {
+                    WAPI.apiCallback(result);
+                });
+            }else{
+                console.log("Socket connection with wearetech server failed.");
+            }
+        }else{
+            console.log("socket.io is not load in this application.");
+        }
+    });
+}
+
+WRTechAPI.prototype.setCloseButtonEventListener = function() {
+    this.closeButton.addEventListener("click", function() {
+        var modaldiv = document.getElementById('wearetech_modal');
+        if(typeof modaldiv !== 'undefined'){
+            document.body.removeChild(modaldiv);
+        }else{
+            console.log("there is not a block div with id='wearetech_modal'");
+        }
+    });
+}
+
+WRTechAPI.prototype.loadScript = function(url, callback){
+    let scrpt = document.createElement('script');
+    scrpt.src = url;
+    scrpt.type = 'text/javascript';
+    scrpt.onload = callback();
+    document.getElementsByTagName('body')[0].appendChild(scrpt);
+}
+
+const WAPI = new WRTechAPI();
+WAPI.setApiButtonEventListener();
+WAPI.loadScript('https://cdn.socket.io/socket.io-1.4.5.js', function(){
+    console.log("Le script a été chargé avec succès.");
+})
---
+++ server.js
@@ -0,0 +1,69 @@
+let express = require('express')
+let http = require('http')
+let app = express()
+let server = http.createServer(app)
+let io = require('socket.io')(server);
+
+//Set ejs as the templates engine
+app.set('view engine', 'ejs')
+
+
+//Middleware pour gerer les requetes ajaxs (de type XMLHttpRequest ou XDomainRequest
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.get('origin'));
+    res.header("Access-Control-Allow-Credentials", true)
+    next();
+});
+//Middleware
+app.use('/assets', express.static(__dirname+'/public'))
+
+app.get('/', (request, response) => {
+    console.log(`new request on the home page url`)
+    response.render('index', {title: "home page"})
+})
+
+app.get('/initpopup/:id', (request, response) => {
+    console.log(request.params.id)
+    console.log(request.get('origin'))
+    response.end(` 
+        <div id='wearetech_modal' style='box-sizing: border-box; display:block;padding-top:10%;padding-left: 35%;background-color: rgba(0, 0, 0, 0.7);z-index: 9999;position: fixed;margin: 0;top: 0;left: 0;bottom: 0;right: 0;'>
+			<div style='overflow:hidden;width: 400px;height:200px;border-radius:6px;background-color: #ffffff;box-shadow:0px 10px 50px rgba(0, 0, 0, 0.8);'>
+				<span style='padding-top: 10px;margin:0;width:100%;height:30px;display:block;position: relative;top: 0;left: 0;right: 0;background-color: #80d6a3;color:#ffffff;text-align: center;border-top-left-radius: 6px;border-top-right-radius: 6px;'>
+				    Total amount: ${request.params.id}
+                </span>
+                <form style='margin:0; display: block;'>
+                    <div style='margin:20px 30px 30px 20px; padding: 0;'>
+                        <label style='display: inline-block; width: 20%; margin: 0; padding: 0; margin: 0;'>Indicatif</label>
+                        <select style='display: inline; width: 75%; margin: 0; padding:0;' id="country_code">
+                            <option>(+237) - Cameroun</option>
+                            <option>(+237) - Gabon</option>
+                            <option>(+237) - République Centrafricaine</option>
+                        </select>
+                    </div>
+                    <div style='margin: 0; margin: 0px 30px 20px 20px; padding: 0;'>
+                        <label style='display: inline-block; width: 20%; margin: 0; padding: 0; margin: 0;'>Tél : </label>
+                        <input type='text' style='display: inline; width: 75%; margin: 0; padding:0;' id='wearetech_phone_number' placeholder='Numéro de téléphone' />
+                    </div>
+                    <div style='text-align: right; margin:0 30px 20px 0px; padding: 0;'>
+                        <button type='button' id="wearetech_validate">Valider</button>
+                        <button type='button' id="wearetech_closemodal">Annuler</button>
+                    </div>
+                </form>
+			</div>
+		</div>
+        
+    `)
+})
+
+//need to be improve to handle client paiement request
+io.on('connection', (socket) => {
+    console.log(`connection to socket.io identifier: ${socket.id}`)
+    socket.on('wearetechapi_client_emit', (data) => {
+        console.log(data)
+        socket.emit('wearetechapi_server_response', {reslt: "happy day"})
+    })
+})
+
+server.listen(process.env.PORT || 5000, () => {
+    console.log(`Server running on port ${process.env.PORT || 5000}`)
+})
