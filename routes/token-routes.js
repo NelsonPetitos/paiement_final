83874eacbd768af8030dd66c93cef2f4bf3cbc22
NelsonPetitos <nde_nelson@yahoo.fr>
2017-02-27 06:16:02 +0100

    T13

--- routes/token-routes.js
+++ routes/token-routes.js
@@ -3,25 +3,66 @@ let router = express.Router();
 let Token = require('../models/token');
 let User = require('../models/user')
 
+let testParamater = function (argument) {
+    // body... 
+    let phoneSchema = /^[1-9][0-9]{8,}/;
+    return( argument.socketid == null || argument.socketid == ''  ||
+            argument.apikey == null ||  argument.apikey == '' ||
+            argument.amount == null || argument.amount == '' ||
+            argument.phone == null ||  argument.phone == '' ||
+            phoneSchema.test(argument.phone) == false))
+}
+
 router.post('/tokens', (request, response) => {
-    let token = new Token();
-    token.phone = request.body.phone;
-    token.amount = request.body.amount;
-    token.apikey = request.body.apikey;
-    token.token = request.body.token;
-    token.socketid = request.body.socketid;
+    let params = {
+        phone: request.body.phone,
+        amount: request.body.amount,
+        apikey: request.body.apikey,
+        socketid: request.body.socketid
+    }
 
-    console.log(request.body);
+    // console.log(request.body);
 
-    if (token.socketid == null || token.apikey == null || token.token == null || token.amount == null || token.phone == null || /^[1-9][0-9]{8,}/.test(token.phone) == false) {
-        response.send({ err: true, msg: "Invalid phone number", data: null })
+    if (testParamater(params)) {
+        // response.send({ err: true, msg: "Invalid phone number", data: null })
+        res.status(200).json({ err: true, msg: "Send valid not empty parameters.", data: null })
     } else {
-        token.save().then((data) => {
-            console.log(`Token save`);
-            response.send({ err: false, msg: 'Token save', data: data });
-        }, (err) => {
-            console.log(`Erreur de sauvegarde du token. ${err}`);
-            response.send({ err: true, msg: 'Save operation fail an error occur.', data: null });
+        // token.save().then((data) => {
+        //     console.log(`Token save`);
+        //     response.send({ err: false, msg: 'Token save', data: data });
+        // }, (err) => {
+        //     console.log(`Erreur de sauvegarde du token. ${err}`);
+        //     response.send({ err: true, msg: 'Save operation fail an error occur.', data: null });
+        // });
+        pg.connect(req.dburl, function(err, client, done) {
+            if(err){
+                console.log('Erreur connection a la bd');
+                console.error(err); 
+                res.status(200).json({ err: true, msg: 'Database connection error.', data: null });
+            }
+            client.query('INSERT INTO tokens(phone, amount, apikey, socketid) VALUES ($1, $2, $3, $4) returning phone, amount, apikey, socketid, token', [params.phone, params.amount, params.apikey, params.socketid], function(err, result) {
+                done();
+                if(err){ 
+                    console.error('Erreur requete'); 
+                    console.log(err);
+                    res.status(200).json({ err: true, msg: 'Token constraints error.', data: null});
+                }
+                
+                // console.log(result.rows);
+                
+                if(result.rows.length === 1){
+                    let data = {
+                        phone: result.rows[0].phone,
+                        amount : result.rows[0].amount,
+                        apikey: result.rows[0].apikey,
+                        token: result.rows[0].token
+                    }
+                    res.status(200).json({ err: false, msg: 'Token create.', data: data });
+                }else{
+                    res.status(200).json({ err: false, msg: 'Multiple results not expected.', data: result.rows});
+                }
+
+            });
         });
     }
 })
