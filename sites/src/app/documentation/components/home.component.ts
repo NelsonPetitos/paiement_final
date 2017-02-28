f6eb4e9af02effeec4536cfc76ecde1508bffe10
NelsonPetitos <nde_nelson@yahoo.fr>
2017-02-28 15:39:26 +0100

    R10

--- sites/src/app/documentation/components/home.component.ts
+++ sites/src/app/documentation/components/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
-    templateUrl: 'home.component.html'
+    // moduleId: module.id,
+    // templateUrl: 'home.component.html'
+    template: `<h1>La documentation</h1>`
 })
 
 export class HomeComponent {
