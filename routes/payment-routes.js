a6b87e1c9e8501061aa1dbd2dca341ee9140cbc6
NelsonPetitos <nde_nelson@yahoo.fr>
2017-03-20 08:08:05 +0100

    A new modification on payment update method

--- routes/payment-routes.js
+++ routes/payment-routes.js
@@ -14,38 +14,42 @@ router.post('', function(req, res){
         message = data.message;
         code = data.code;
     }
-    switch (field) {
-        case 'status_receive':
-            sqlString = 'update payments set  status_receive = true, date_receive = now() where token_id = $1';
-            break;
-        case 'status_send':
-            sqlString = 'update payments set  status_send = true, date_send = now() where token_id = $1';
-            break;
-        default:
-            sqlString = '';
-            break;
-    }
-    if(req.dburl){
-        pg.connect(req.dburl, function(err, client, done) {
-            if(err){
-                console.log('Erreur connection a la bd : payment-routes');
-                console.error(err); 
-                return res.status(500).json({ err: true, msg: 'Database connection error.'});
-            }
-            client.query(sqlString, [token, status_payment, code, message], function(err, result) {
-                done();
-                if(err){ 
-                    console.error('Erreur requete : payment-routes'); 
-                    console.log(err);
-                    return res.status(500).json({ err: true, msg: 'Query error.'});
+    if(field && token){
+        switch (field) {
+            case 'status_receive':
+                sqlString = 'update payments set  status_receive = true, date_receive = now() where token_id = $1';
+                break;
+            case 'status_send':
+                sqlString = 'update payments set  status_send = true, date_send = now() where token_id = $1';
+                break;
+            default:
+                break;
+        }
+        if(req.dburl){
+            pg.connect(req.dburl, function(err, client, done) {
+                if(err){
+                    console.log('Erreur connection a la bd : payment-routes');
+                    console.error(err); 
+                    return res.status(500).json({ err: true, msg: 'Database connection error.'});
                 }
-                console.log(result.rows[0]);
-                return res.status(200).json({ err: false, msg: 'Payment update.', data: result.rows[0] });
+                client.query(sqlString, [token, status_payment, code, message], function(err, result) {
+                    done();
+                    if(err){ 
+                        console.error('Erreur requete : payment-routes'); 
+                        console.log(err);
+                        return res.status(500).json({ err: true, msg: 'Query error.'});
+                    }
+                    console.log(result.rows[0]);
+                    return res.status(200).json({ err: false, msg: 'Payment update.', data: result.rows[0] });
+                });
             });
-        });
+        }else{
+            console.log('unable to find database url : payment-routes');
+            return res.status(500).json({err: true, msg: 'Database url missing.'});
+        }
     }else{
-        console.log('unable to find database url : payment-routes');
-        return res.status(500).json({err: true, msg: 'Database url missing.'});
+        console.log('Bad request : payment-routes');
+        return res.status(404).json({err: true, msg: 'Bad request.'});
     }
 })
 
